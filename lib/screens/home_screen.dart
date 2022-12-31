f5be86bbb0bdc0b162650c444b41c73de30573d5
fikriymnn <fikriyomann@student.upi.edu>
2022-12-31 19:13:51 +0700

    update home_screen, nambah asset image + package

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,46 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: screenSize.width,
+      height: screenSize.height,
+      decoration: const BoxDecoration(
+        color: Color(0xff013088),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                width: screenSize.width,
+                height: 558,
+                margin: const EdgeInsets.only(top: 142),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/images/image_1.png',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 226,
+            height: 224.06,
+            margin: const EdgeInsets.only(left: 79),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/logo_banner.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
