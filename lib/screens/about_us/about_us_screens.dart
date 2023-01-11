615462eca6e416ef54901c85493e6f3f2ba7d971
ramahartadi <ramahartadi51@gmail.com>
2023-01-11 11:09:01 +0700

    WebSmoothScroll untuk about us, contact us, dan solution

--- lib/screens/about_us/about_us_screens.dart
+++ lib/screens/about_us/about_us_screens.dart
@@ -6,6 +6,7 @@ import 'package:saptaloka_web_revamp/screens/about_us/widget/visi_misi.dart';
 import 'package:saptaloka_web_revamp/screens/about_us/widget/weare_wedo.dart';
 import 'package:saptaloka_web_revamp/widgets/footer.dart';
 import 'package:saptaloka_web_revamp/widgets/header_widget.dart';
+import 'package:web_smooth_scroll/web_smooth_scroll.dart';
 
 class AboutUsScreens extends StatefulWidget {
   static const routeName = '/AboutUs';
@@ -16,6 +17,20 @@ class AboutUsScreens extends StatefulWidget {
 }
 
 class _AboutUsScreensState extends State<AboutUsScreens> {
+  late ScrollController _scrollController;
+  double pixels = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      setState(() {
+        pixels = _scrollController.position.pixels;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
@@ -25,26 +40,30 @@ class _AboutUsScreensState extends State<AboutUsScreens> {
         preferredSize: Size(screenSize.width, 70),
         child: HeaderWidget(),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: const [
-            StorySaptaloka(),
-            SizedBox(
-              height: 50,
-            ),
-            WeAreWedo(),
-            SizedBox(
-              height: 50,
-            ),
-            VisiMisi(),
-            SizedBox(
-              height: 50,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Footer()
-          ],
+      body: WebSmoothScroll(
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Column(
+            children: const [
+              StorySaptaloka(),
+              SizedBox(
+                height: 50,
+              ),
+              WeAreWedo(),
+              SizedBox(
+                height: 50,
+              ),
+              VisiMisi(),
+              SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Footer()
+            ],
+          ),
         ),
       ),
     );
